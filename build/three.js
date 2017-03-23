d4194c435bfa6d739075d627c446e2f869383580
satori99 <satori.developer@gmail.com>
2017-03-23 20:53:47 +1100

    Update build

--- build/three.js
+++ build/three.js
@@ -194,7 +194,7 @@
 
 	} );
 
-	var REVISION = '85dev';
+	var REVISION = '85log-depth-shadows';
 	var MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2 };
 	var CullFaceNone = 0;
 	var CullFaceBack = 1;
--- build/three.module.js
+++ build/three.module.js
@@ -188,7 +188,7 @@ Object.assign( EventDispatcher.prototype, {
 
 } );
 
-var REVISION = '85dev';
+var REVISION = '85log-depth-shadows';
 var MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2 };
 var CullFaceNone = 0;
 var CullFaceBack = 1;
